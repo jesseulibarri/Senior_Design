87c62b09551c6b23026e6c368662ec657d0d11dd
ulibarje <ulibarje@oregonstate.edu>
2016-12-22 09:56:13 -0800

    Working on sending the speed to RPi

--- Simulations/Calc_Speed2/calc_speed2.c
+++ Simulations/Calc_Speed2/calc_speed2.c
@@ -15,24 +15,132 @@
 #include <stdlib.h>
 #include "../Extern_Files/hd44780.h"
 #define PI 3.14159
-#define count_period 0.016
+#define count_period    0.016
 
+//Function Headers
 void real_clk_init();
-void SPI_init();
+void spi_init();
+void spi_init_master();
 void timer1_init();
+void format_lcd_array(double);
+void calc_speed();
+void spi_16bit_transmit(uint16_t);
+void spi_double_transmit(double);
 
+//Global Variables
 volatile uint16_t i = 0;          //index
 uint8_t tire_diam = 22;
 uint8_t sprocket_teeth = 42;
 double tire_circ;
 double distance_per_pulse;
-double seconds = 0;
-uint16_t timestamp;
 double speed;
 
 char lcd_string[32] = {"                                "};
 char char_numbers[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
+
+ISR(TIMER1_CAPT_vect) {
+
+    PORTC |= (1 << PC1);
+    calc_speed();
+    spi_double_transmit(speed);
+    
+    if(i > 100) {
+        format_lcd_array(speed);
+        i = 0;
+    }
+    i++;
+   
+    PORTC &= ~(1 << PC1);
+
+}
+
+int main()
+{
+
+tire_circ = tire_diam * PI;
+distance_per_pulse = tire_circ / sprocket_teeth;
+
+DDRC |= (1 << PC0) | (1 << PC1);;     //for troubleshooting
+
+//initializations
+timer1_init();
+
+/****** Turn on for datalogging *******/
+//spi_init();
+
+/****** Turn on to send to lcd *******/
+spi_init_master();
+lcd_init();
+clear_display();
+
+sei();                  //enable global interrupts
+
+while(1) {
+    
+    /****** Turn on to send to lcd *******/
+    refresh_lcd(lcd_string);
+    _delay_us(50);
+}
+
+return 0;
+}//main
+
+
+/*****************************************************************************
+ * Name: spi_init_master
+ *  
+ * Description: This function intializes the data direction and SPI registers
+ *  for SPI transmission in master mode, with rising edge sample.
+ * **************************************************************************/
+void spi_init_master() {
+	// set up SPI (master mode, clk low on idle, leading edge sample)
+	SPCR = (1 << SPE) | (1 << MSTR) | (0 << CPOL) | (0 << CPHA);
+	SPSR = (1 << SPI2X);
+}//SPI_init
+
+
+/*****************************************************************************
+ * Name: spi_init
+ *  
+ * Description: This function intializes the data direction and SPI registers
+ *  for SPI transmission in slave mode, with rising edge sample.
+ * **************************************************************************/
+void timer1_init() {
+
+    //Makes use of the input capture function on PORTD.4.
+    TCCR1A = 0x00;                          //Normal mode, no compare
+    TCCR1B |= (1 << ICES1) | (1 << CS12);   //Input capture on rising edge,
+                                            //256 clk prescale
+    TIMSK |= (1 << TICIE1);                 //Enable input capture interrupt
+
+}//timer1_init
+
+
+/*****************************************************************************
+ * Name: spi_init
+ *  
+ * Description: This function intializes the data direction and SPI registers
+ *  for SPI transmission in slave mode, with rising edge sample.
+ * **************************************************************************/
+void spi_init(){
+    
+    //Set MOSI, SCK as output
+    DDRB |= (1<<PB3);
+    //Configure SPI (Slave mode, clk low on idle, rising edge sample)
+    SPCR = (1<<SPE)|(0<<MSTR)|(0<<CPOL)|(0<<CPHA); 
+    SPSR = (1<<SPR1)|(1<<SPR0);
+
+}//spi_init
+
+
+/**************************************************************************************
+ * Name: format_lcd_array
+ *
+ * Description: Takes a double and breaks it up into two parts (16-bit integer part and
+ * 16-bit fraction part. The it converts the int values into ASCII values and builds
+ * the LCD array to update.
+ *************************************************************************************/
 void format_lcd_array(double number) {
     uint16_t integer_part = (uint16_t)number;
     uint16_t fraction_part = 10000 * (number - integer_part);
@@ -45,101 +153,62 @@ void format_lcd_array(double number) {
     lcd_string[5] = char_numbers[(fraction_part / 10) % 10];
     lcd_string[6] = char_numbers[(fraction_part / 1) % 10];
                                               
-}
+}//format_lcd_array
 
 
+/**************************************************************************************
+ * Name: calc_speed
+ *
+ * Description: Calculates the speed based on the incoming speed sensor pulse.
+ *************************************************************************************/
 void calc_speed() {
 
     PORTC |= (1 << PC0);
+    uint16_t timestamp = ICR1;
     static uint16_t timestamp_hist = 0;
-    uint16_t difference;
     uint16_t timestamp_dif;
 
     if(timestamp < timestamp_hist) {
-        difference = 65535 - timestamp_hist;
-        timestamp_dif = difference + timestamp;
+        timestamp_dif= 65535 - timestamp_hist + timestamp;
     } 
     else 
         timestamp_dif = timestamp - timestamp_hist;
 
     double msec = (double)timestamp_dif * count_period;
     double seconds = msec / 1000;
-    speed = (distance_per_pulse / seconds) * (1 / 17.6);
+    speed = (distance_per_pulse/ seconds) * (1 / 17.6);
     timestamp_hist = timestamp;
     PORTC &= ~(1 << PC0);
 
 }//calc_speed
 
+/**************************************************************************************
+ * Name: spi_16bit_transmit
+ *
+ * Description: This function is passed a 16 bit integer and breaks it into a high and 
+ *  low byte to be sent over SPI.
+ *************************************************************************************/
+void spi_16bit_transmit(uint16_t result){
+    char uint16_low = result & 0xFF;
+    char uint16_high = result >> 8;
 
-ISR(TIMER1_CAPT_vect) {
+    SPDR = uint16_high;                    //Load high byte into SPDR buffer
+    while(bit_is_clear(SPSR, SPIF)){}   //Wait till SPI data has been sent out
+    SPDR = uint16_low;                     //Load low byte into SPDR buffer
+    while(bit_is_clear(SPSR, SPIF)){}   //Wait till SPI data has been sent out
 
-    PORTC |= (1 << PC1);
-    timestamp = ICR1;
-    calc_speed();
-    if(i > 100) {
-        format_lcd_array(speed);
-        i = 0;
-    }
-    i++;
-    PORTC &= ~(1 << PC1);
+}//spi_16bit_transmit
 
-}
+/******************************************************************************
+ * Name: spi_double_transmit
+ *
+ * Description: This function is passed a double and breaks it into 4 bytes to
+ *  be sent one byte at a time over SPI.
+ ******************************************************************************/
+void spi_double_transmit(double number){
+    uint16_t integer_part = (uint16_t)number;
+    uint16_t fraction_part = 1000 * (number - integer_part);
 
-int main()
-{
-tire_circ = tire_diam * PI;
-distance_per_pulse = tire_circ / sprocket_teeth;
-
-DDRC |= (1 << PC0) | (1 << PC1);;     //for troubleshooting
-
-DDRB = 0xFF;
-
-//real_clk_init();        //initialize clock
-timer1_init();
-SPI_init();
-lcd_init();
-clear_display();
-sei();                  //enable global interrupts
-
-while(1) {
-    
-    refresh_lcd(lcd_string);
-    _delay_us(50);
-}
-
-return 0;
-}//main
-
-
-void real_clk_init() {
-    
-    // Follow procedures in the datasheet to select the external clock.
-    TIMSK &= ~((1 << OCIE0) | (1 << TOIE0)); //clear interrupts
-    ASSR |= (1 << AS0);                     //enable external clock
-    TCCR0 = (1 << WGM01) | (0 << WGM00) | \
-            (0 << CS01) | (1 << CS00);      //CTC mode, 8 prescale
-    OCR0 = 32;                               //set TOP to measure mS
-    while(!((ASSR & 0b0111) == 0)) {}       //spin till registers finish updating
-    TIFR |= (1 << OCF0) | (1 << TOV0);      //clear interrupt flags
-    TIMSK |= (1 << OCIE0);                  //enable compare match interrupt
-
-}//real_clk_intit
-
-
-void SPI_init() {
-	// set up SPI (master mode, clk low on idle, leading edge sample)
-	SPCR = (1 << SPE) | (1 << MSTR) | (0 << CPOL) | (0 << CPHA);
-	SPSR = (1 << SPI2X);
-}//SPI_init
-
-
-void timer1_init() {
-
-    //Makes use of the input capture function on PORTD.4.
-    TCCR1A = 0x00;                          //Normal mode, no compare
-    TCCR1B |= (1 << ICES1) | (1 << CS12);   //Input capture on rising edge,
-                                            //256 clk prescale
-    TIMSK |= (1 << TICIE1);                 //Enable input capture interrupt
-
-}//timer1_init
-
+    spi_16bit_transmit(integer_part);
+    spi_16bit_transmit(fraction_part);
+}//spi_double_transmit

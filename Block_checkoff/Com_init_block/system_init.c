aea48ab90c04933688cd285b4cb96d523e8800ba
Jesse Ulibarri <jesseulibarri@jesse-think.jesseulibarri>
2017-01-14 12:02:12 -0800

    Added a Block Checkoff folder for all checkoff components

--- Block_checkoff/Com_init_block/system_init.c
+++ Block_checkoff/Com_init_block/system_init.c
@@ -1,26 +1,48 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdlib.h>
+#include <util/delay.h>
 #include "math.h"
-#include "system_init.h"
-#include "pirate.h"
-#include "const.h"
-//TODO: #include "speed.h"
-//TODO: #include "datalogging.h"
 
-/*
+#define F_CPU 16000000
+#define ON  1
+#define OFF 0
+#define TIRE_DIAM       22
+#define SPROCKET_TEETH  42
+#define SPEED1_RELAY    4
+#define SPEED2_RELAY    5
+#define PC_RELAY        6
+#define PIRATE_SWITCH   7
+
 //Global Variables
-uint8_t tire_diam = 22;
-uint8_t sprocket_teeth = 42;
+//uint8_t tire_diam = 22;
+//uint8_t sprocket_teeth = 42;
 double tire_circ;
 double distance_per_pulse;
-double speed;
-uint8_t dropped_byte = 0;
-*/
+//double speed;
 
 /*** Turn ON to enable datalogging ***/
-uint8_t datalogging = OFF;
+uint8_t datalogging = ON;
 
+/********************************************************
+ * Name: send_speed
+ *
+ * Description: This is not the completed "send speed"
+ *  function. This is needed to verify the block 
+ *  according to the testing proceedures in the block 
+ *  description report.
+ * ******************************************************/
+void send_speed() {
+
+    // For block checkoff testing proceedures
+    PORTC &= ~(1 << PC0);   //end timing test
+
+    // For block checkoff, send theoretical speed
+    //UDR0 = 10;      //send a speed of 10mph
+    //while(bit_is_clear(UCSR0A, TXC)) {} //wait for tx to complete
+
+}//send_speed
 
 /********************************************************
  * Name: system_init
@@ -51,7 +73,7 @@ void system_init() {
     /******** System Timer *********/
     //Timer will run on the 32kHz oscillator to allow for a slower speed
     //Normal mode, 32,768Hz with 8 pre-scale = 16Hz = 62.5mS
-    
+
     // Follow procedures in the datasheet to select the external clock.
     TIMSK &= ~((1 << OCIE0) | (1 << TOIE0)); //clear interrupts
     ASSR |= (1 << AS0);                      //enable external clock
@@ -63,13 +85,13 @@ void system_init() {
 
 
     /******** Enable Global Interrupts *********/
-    sei();
+    //sei();
 
     /******** IO *********/
     DDRB |= (1<<SPEED1_RELAY)|(1<<SPEED2_RELAY)|(1<<PC_RELAY); //Output for relay circuits
-    DDRD |= (1<<PIRATE_SWITCH);     //Pirate mode enable on PORTD.0 (INT0)
+    DDRB |= (1<<PIRATE_SWITCH);     //Pirate mode enable on PORTD.0 (INT0)
     PORTB |= (1<<SPEED1_RELAY)|(1<<SPEED2_RELAY)|(1<<PC_RELAY); //Turn on relay circuits
-    PORTD |= (1<<PIRATE_SWITCH);     //Set high
+    PORTB |= (1<<PIRATE_SWITCH);     //Set high
 
     ///*** Calculate the system needed constants ***/
     tire_circ = TIRE_DIAM * M_PI;
@@ -79,9 +101,9 @@ void system_init() {
     if(datalogging) { 
 
         //Set MOSI, SCK as output
-        DDRB |= (1<<PB3);
+        DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2);
         //Configure SPI (Slave mode, clk low on idle, rising edge sample)
-        SPCR = (1<<SPE)|(0<<MSTR)|(0<<CPOL)|(0<<CPHA)|(1<<SPR1)|(0<<SPR0);
+        SPCR = (1<<SPE)|(1<<MSTR)|(0<<CPOL)|(0<<CPHA)|(1<<SPR1)|(0<<SPR0);
         SPSR = (1<<SPI2X);
 
     }//if datalogging
@@ -101,32 +123,51 @@ void system_init() {
     UCSR0B |= (1 << RXEN0) | (1 << TXEN0);      //Enable TX, RX
     UCSR0C |= (0<<UMSEL0)|(0<<USBS0)|(1<<UCSZ00)|(1<<UCSZ01);   //Async, no parity, 1 stop bit
                                                                 // 8-bit char size
+    UBRR0H = (51 >> 8);     //set baudrate to 38.4k
+    UBRR0L = 51;
 
     /****** UART2 *******/
     //TODO: UART interrupts
     UCSR1B |= (1 << RXEN1) | (1 << TXEN1);      //Enable TX, RX
     UCSR1C |= (0<<UMSEL1)|(0<<USBS1)|(1<<UCSZ10)|(1<<UCSZ11);   //Async, no parity, 1 stop bit
                                                                 // 8-bit char size
-    
+    UBRR1H = (51 >> 8);      //set baudrate to 38.4k
+    UBRR1L = 51;
+  
     //TODO: Add any timers/interrupts found to be necessary!!!!
     
     /****** For Block Checkoff *******/
     //Send a value via UART
-    UDR0 = 5;
-    while(bit_is_clear(UCSR0A, TXC)) {} // wait until data is sent
+    //UDR0 = 5;
+    //while(bit_is_clear(UCSR0A, TXC)) {} // wait for tx to complete
 
     //Send a value to the bar graph via SPI
-    DDRE = (1 << PE1);  //set data direction for bar graph poke
+    DDRE = (1 << PE6) | (1 << PE5);  //set data direction for bar graph poke
     //*********** Send SPI Data **********
-    SPDR = 5;
+    SPDR = 6;
     while(bit_is_clear(SPSR, SPIF)) {} // wait until data is sent
     
     //********** Bar Graph Portion *******************
     PORTE |= (1 << PE6);      // move graph data from shift to storage reg.
     PORTE &= ~(1 << PE6);     // change 3-state back to high Z
 
+    //PORTC &= ~(1 << PC0);   //end timing test
+    send_speed();
+
     /******************** End Testing **************************/
 
-    PORTC &= ~(1 << PC0);   //end timing test
 
 }//system_init
+
+int main() {
+
+DDRB |= (1 << PB7);
+system_init();
+
+while(1) {
+PORTB ^= (1 << PB7);
+_delay_ms(1000);
+}
+
+return 0;
+}//main

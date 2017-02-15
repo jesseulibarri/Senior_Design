235ca4e538b59f08893cf784e46b346c3b1a1c57
Jesse Ulibarri <jesseulibarri@jesse-think.jesseulibarri>
2017-02-14 23:33:34 -0800

    Added a generic uart_send_garbage program to test communication between microcontroller and computer

---
+++ Simulations/uart_send_garbage/uart_send_garbage.c
@@ -0,0 +1,29 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "uart_functions.h"
+#include <util/delay.h>
+
+
+
+
+int main() {
+DDRB = 0xFF;
+    
+uart1_init();
+
+while(1) {
+
+    //Send a value via UART
+    uint8_t i;
+    for(i = 0; i < 10; i++) {
+        PORTB = 0x01;
+        while (!(UCSR1A&(1<<UDRE1)));    // Wait for previous transmissions
+            UDR1 = i;    // Send data byte
+        while (!(UCSR1A&(1<<UDRE1)));    // Wait for previous transmissions
+        _delay_ms(100);
+        PORTB = 0x00;
+    }//for
+
+}//while
+}//main

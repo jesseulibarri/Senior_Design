1511b82cda358a58e6c4b6faea3d82f7a073a2ba
Jesse Ulibarri <jesseulibarri@jesse-think.jesseulibarri>
2017-02-15 01:01:01 -0800

    Debugged the motor torque algorithm. Everything compiles, but program does not work yet.

--- Block_checkoff/Motor_Speed_Algorithm/torque.c
+++ Block_checkoff/Motor_Speed_Algorithm/torque.c
@@ -24,8 +24,8 @@
 #define pirate_switch 0     //Port D Pin 0
 
 //Global Torque Variables
-float torque_right = 0;
-float torque_left = 0;
+float torque_right = 0.0;
+float torque_left = 0.0;
 
 /****************************************************************
  * Name: timer1_init
@@ -41,18 +41,13 @@ void timer1_init(){
     TCCR1B = (1<<CS11)|(1<<CS10);
 
     //Set Output Comare Match A Value
-    OCR1A = 2499; 
+    OCR1A = 24999; 
 
     //Configure Timer/Counter 1 Output Compare Match A Interrupt
-    TIMSK = (1<<OCIE1A);
+    TIMSK = (1<<TOIE1);
 
 }//timer1_init
 
-ISR(TIMER1_COMPA_vect){
-motor_torque(&torque_right, &torque_left);
-spi_init();
-
-}//timer1_isr
 
 /****************************************************************
  * Name: spi_encoder_init
@@ -63,7 +58,7 @@ spi_init();
 void spi_encoder_init(){
 
     //Set data direction for SPI and set pullup for MISO
-    DDRB = (1<<PB0)|(1<<PB1)|(1<<PB2)|(0<<PB3);
+    DDRB |= (1<<PB0)|(1<<PB1)|(1<<PB2)|(0<<PB3);
     PORTB |= (1<<PB3); //MISO line
     
 
@@ -107,6 +102,7 @@ uint16_t get_angle(){
     uint8_t high_byte;
     uint8_t low_byte;
     uint16_t angle;
+    uint8_t i;
 
     spi_encoder_init();
 
@@ -124,7 +120,18 @@ uint16_t get_angle(){
         while(bit_is_clear(SPSR, SPIF)){}
         PORTD |= (1<<PD0);      //Set Select Line High
         _delay_us(20);          //Wait
-    }
+        if(i == 10) {
+            PORTA |= (1<<PA1);
+            PORTD &= ~(1<<PD0);     //set select line low
+            SPDR = rd_pos;          //send get position command
+            while(bit_is_clear(SPSR, SPIF)) {}
+            PORTD |= (1<<PD0);      //set select line high
+            PORTA &= ~(1<<PA1);
+            _delay_us(20);
+        }//if
+    i++;
+
+    }//while
 
     //Encoder is ready, read the upper byte (top 4 bits of the 12 total)
     PORTD &= ~(1<<PD0);     //Set Select Line Low
@@ -135,14 +142,15 @@ uint16_t get_angle(){
     _delay_us(20);          //Wait
 
     PORTD &= ~(1<<PD0);     //Set Select Line Low
-    SPDR = nop_a5           //Send no-op
+    SPDR = nop_a5;           //Send no-op
     while(bit_is_clear(SPSR, SPIF)){}   //Wait for Position to Be received
     PORTD |= (1<<PD0);      //Set Select Line High
     low_byte = SPDR;
 
     angle = (high_byte<<8)|(low_byte);
+    spi_init();
     return angle;
-}
+}//get_angle
 
 
 
@@ -151,22 +159,25 @@ uint16_t get_angle(){
  *
  * Description: 
  ****************************************************************/
-void motor_torque(float *Torque_right, float *Torque_left){
+void motor_torque(float* torque_right, float* torque_left){
     
     uint16_t angle;
     float torque_ratio;
-    static float general_torque += 0.5;
+    static float general_torque;
+    general_torque = general_torque + 0.5;
     static uint8_t max_torque = 25;
-    
+   
+    uint8_t user_mode = PIND || 0x7F;
+
     angle = get_angle();
 
     switch(user_mode){
 
         //No Acceleration, Ramp 
         case 0x00:
-
+            break;
         //Accelerating Mode, Ramp Up
-        case 0x01:
+        case 0x7F:
             //Prevent torque from exceeding max torque
             if(general_torque > max_torque){
                 general_torque = max_torque;
@@ -174,20 +185,23 @@ void motor_torque(float *Torque_right, float *Torque_left){
             //We are turning right
             if(angle >= 0 && angle <= 2048){
                 torque_ratio = ((-0.00031)*angle)+(0.99972);
-                torque_right = general_torque*torque_ratio;
-                torque_left = general_torque;
+                *torque_right = general_torque*torque_ratio;
+                *torque_left = general_torque;
             }
             //We are turning left
             else{
                 torque_ratio = ((1.033)*log((double)angle))-(7.59);
-                torque_left = general_torque*torque_ratio;
-                torque_right = general_torque;
+                *torque_left = general_torque*torque_ratio;
+                *torque_right = general_torque;
             }
-
+            break;
         //Cruise Mode
         case 0x03:
+            break;
 
-}
+    }//switch
+}//motor_torque
+
 
 void pirate_mode(){
 
@@ -202,7 +216,7 @@ void pirate_mode(){
     sleep_enable(); //Set sleep enable bit in MCUCR register
     sei();          //Set global interrupt bit
     sleep_cpu();    //CPU is sleeping
-    sleep_disable   //CPU wakes up on rising edge ISR is executed
+    sleep_disable();   //CPU wakes up on rising edge ISR is executed
 
     PORTB |= (1<<speed1_relay)|(1<<speed2_relay)|(1<<pc_relay); //Turn on relay circuits
 }//pirate_mode
@@ -211,19 +225,33 @@ ISR(INT0_vect){
     EIMSK &= ~(1<<INT0);
 }//ISR
 
+ISR(TIMER1_OVF_vect){
+    PORTB ^= (1<<PB7);
+    motor_torque(&torque_right, &torque_left);
+    //spi_init();
+
+}//timer1_isr
+
+
 
 int main(){
 
     uint16_t TR;
     uint16_t TL;
-    char lcd_data1[16] = {"             "};
+    char lcd_data1[16] = {"         "};
     char lcd_data2[16] = {"             "};
     char numbers[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-    DDRD &= ~(1<<PD0); //Configure Port D Pin 0 for input
+    DDRB |= (1<<PB7)|(1<<PB6);
+
+    DDRD |= (1<<PD0);   //SPI SS pin
+    DDRD &= ~(1<<PD7); //Configure Port D Pin 7 for input
+    PORTD |= (1<<PD7);  //enable pullup
+    timer1_init();
     spi_init();
     lcd_init();
     _delay_ms(200);
+    sei();
 
     while(1){
         TR = spi_float_to_int(torque_right);
@@ -232,27 +260,28 @@ int main(){
         clear_display();
         cursor_home();
 
+        //PORTB ^= (1<<PB7);
+
         lcd_data1[0] = numbers[TR/1000 % 10];
         lcd_data1[1] = numbers[TR/100 % 10];
         lcd_data1[2] = numbers[TR/10 % 10];
         lcd_data1[3] = numbers[TR % 10];
 
-        lcd_data2[2] = numbers[TL/1000 % 10];
-        lcd_data2[3] = numbers[TL/100 % 10];
-        lcd_data2[4] = numbers[TL/10 % 10];
-        lcd_data2[5] = numbers[TL % 10];
+        lcd_data2[0] = numbers[TL/1000 % 10];
+        lcd_data2[1] = numbers[TL/100 % 10];
+        lcd_data2[2] = numbers[TL/10 % 10];
+        lcd_data2[3] = numbers[TL % 10];
         
-        sting2lcd(lcd_data1);
-        sting2lcd(lcd_data2);
+        string2lcd(lcd_data1);
+        string2lcd(lcd_data2);
 
-        _delay_ms(100);
-        clear_display();
-
+//        _delay_ms(100);
+/*
         if(PIND & (1<<PD0)){
             pirate_mode();
             _delay_ms(10);
-        }
-    }
-    return 0;
-}
+        }*/
+    }//while
+return 0;
+}//main
 

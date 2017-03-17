d22d2aed01239567cc515f8e28f76ea3cc43a0e9
eligilbert12 <eliyazzolino@yahoo.com>
2017-03-16 19:35:36 -0700

    Finished tweaking the torque algorithm.

--- Block_checkoff/data_logging2/torque.c
+++ Block_checkoff/data_logging2/torque.c
@@ -188,7 +188,7 @@ void motor_torque(float* torque_right, float* torque_left, uint16_t* steer_angle
     uint8_t user_mode = PIND | 0x7F;
 
     general_torque += 0.5;	//This variable is used for the ramping feature
-    angle = get_angle();	//get steering angle reading from encoder
+    angle = 0;	//get steering angle reading from encoder
     //angle = 10;
     *steer_angle = angle;	//modify the global steering angle variable
     
@@ -356,8 +356,8 @@ ISR(TIMER1_OVF_vect){
     float_to_bytes(&steering_angle_float, steering_angle_bytes);
 
     uart_transmit(torque_r_bytes,4);		//transmit right torque value - float, 4 bytes
-    uart_transmit(torque_l_bytes,4);    		//transmit left torque value - float, 4 bytes
-    uart_transmit(steering_angle_bytes,4);		//transmit steering encoder value - uint16, 2 bytes
+    //uart_transmit(torque_l_bytes,4);    		//transmit left torque value - float, 4 bytes
+    //uart_transmit(steering_angle_bytes,4);		//transmit steering encoder value - uint16, 2 bytes
 
     spi_init();					//Used to initalize SPI for LCD screen if being used
     PORTF &= ~(1<<PF0);

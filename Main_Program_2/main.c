65e74046eac8d89e56d9ae061f3ddedf32d63e30
28licaris <shanelicari28@gmail.com>
2017-03-18 08:51:51 -0700

    added a new main folder to work out of

---
+++ Main_Program_2/main.c
@@ -0,0 +1,29 @@
+/********************************************************
+ * File Name: main.c
+ * Author:
+ * Date:
+ *
+ * Description: This is the main loop for the SEM Urban
+ *  Concept vehicle.
+ *  ****************************************************/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "system_init.h"
+#include "uart.h"
+
+
+int main()
+{
+
+system_init();
+
+while(1) {
+	
+}
+
+return 0;
+}//main

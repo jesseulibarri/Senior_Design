5d01ab5958574eec41281235159d6905fe55cdea
Eligilbert12_Desktop <eliyazzolino@yahoo.com>
2017-03-19 16:42:50 -0700

    Added program where UART Recieve suposely worked...

---
+++ Main_Program_Working/conversions.c
@@ -0,0 +1,36 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include "conversions.h"
+
+
+/**************************************************************************************
+ * Name: bytes_to_float
+ *
+ * Description: 
+ *************************************************************************************/
+void bytes_to_float(char* src, float* dest) {
+    union {
+        float a;
+        char bytes[4];
+    } u;
+    memcpy(u.bytes, src, 4);
+    *dest = u.a;
+}
+
+
+/**************************************************************************************
+ * Name: float_to_bytes
+ *
+ * Description: 
+ *************************************************************************************/
+void float_to_bytes(float* src, unsigned char* dest) {
+    union {
+        float a;
+        unsigned char bytes[4];
+    } u;
+    u.a = *src;
+    memcpy(dest, u.bytes, 4);
+}//float_to_bytes
+
+

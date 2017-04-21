dea755bb457ced4a856f6dee72d28dde2f44e757
Eligilbert12_Desktop <eliyazzolino@yahoo.com>
2017-04-21 12:16:46 -0700

    Almost Finished with VESC Prototype Program

---
+++ Prototype_Main_Program - 2/conversions.h
@@ -0,0 +1,15 @@
+/*******************************************************
+ * File Name: conversions.h
+ * Authors: Jesse Ulibarri, Shane Licari, Eli Yazzolino
+ * Date: 4/20/2017
+ *
+ * Description:
+********************************************************/
+
+#ifndef CONVERSIONS_H
+#define CONVERSIONS_H
+
+void bytes_to_float(unsigned char* src, float* dest);
+void float_to_bytes(float src, unsigned char* dest);
+
+#endif
---
+++ Prototype_Main_Program - 2/main.c
@@ -0,0 +1,24 @@
+/*******************************************************
+ * File Name: main.c
+ * Authors: Jesse Ulibarri, Shane Licari, Eli Yazzolino
+ * Date: 4/20/2017
+ *
+ * Description: This is the main loop for the SEM Urban
+ * Concept vehicle.
+ *****************************************************/
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "system_init.h"
+#include "uart.h"
+
+int main(){
+	//Initialize the system
+	system_init();
+	//delay for 1 sec to ensure motor controller is functional
+	_delay_ms(1000)
+	
+	while(1){
+	}
+	return 0;
+}//main

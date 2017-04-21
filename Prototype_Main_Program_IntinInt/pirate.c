a76d8524d6004b8f57e650a13a15a4d8172d6a05
Eligilbert12_Desktop <eliyazzolino@yahoo.com>
2017-04-21 14:52:35 -0700

    Moving the interupt to the main function.

---
+++ Prototype_Main_Program_IntinInt/pirate.c
@@ -0,0 +1,46 @@
+/*******************************************************
+ * File Name: pirate.c
+ * Authors: Jesse Ulibarri, Shane Licari, Eli Yazzolino
+ * Date: 4/20/2017
+ *
+ * Description: File contains low-power mode ISR Function
+********************************************************/
+
+//#define F_CPU 16000000
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include "pirate.h"
+#include "system_init.h"
+
+/*********************************************************************
+ * Name: pirate_mode
+ *
+ * Description: This function will be called when the pirate mode
+ * switch is toggled to logic low. Then the speed 1 & 2 and 12v 
+ * relay will be turned off. A while loop will execute until a
+ * logic high is detected on the pirate switch.Then before returing
+ * to main the three relays will be turned back on. 
+ *
+ * TODO: Configure Port C data direction for pirate mode function.
+ *********************************************************************/
+void pirate_mode(){
+    //Configure interrupt 0 so a rising edge will wake up the controller out of sleep mode
+    EICRA = (1<<ISC00) | (1<<ISC01);     //Generate aysnchronous interrupt request on rising edge
+    EIMSK = (1<<INT4);                   //Enable external interrupt 0
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Enable power down mode, set sleep enable bit 
+                                         //in the MCUCR register
+    PORTD &= ~(1<<PC_ON_OFF);            //Turn 12V power converter
+    PORTB |= (1<<PB7);
+    cli();                               //clear global interrupt, only needed if storing data before sleep
+    sleep_enable();                      //Set sleep enable bit in MCUCR register
+    sei();                               //Set global interrupt bit
+    sleep_cpu();                         //CPU is sleeping
+    sleep_disable();                     //CPU wakes up on rising edge ISR is executed
+
+    system_init();
+
+}//pirate_mode
+
---
+++ Prototype_Main_Program_IntinInt/system_init.h
@@ -0,0 +1,29 @@
+/*******************************************************
+ * File Name: system_init.h
+ * Authors: Jesse Ulibarri, Shane Licari, Eli Yazzolino
+ * Date: 4/20/2017
+ *
+ * Description: Header file for the system 
+ * initialization.
+********************************************************/
+
+#ifndef SYSTEM_INIT
+#define SYSTEM_INIT
+
+#define ON  1
+#define OFF 0
+#define ACCELERATE_B    7
+#define PIRATE_SWITCH   0
+#define PC_ON_OFF       5
+
+#define USART_BAUDRATE 76800  
+#define BAUDVALUE  ((F_CPU/(USART_BAUDRATE * 16UL)) - 1 )
+
+#define USART1_BAUDRATE 115200  
+#define BAUDVALUE_1  ((F_CPU/(USART1_BAUDRATE * 16UL)) - 1 )
+
+void system_init();
+
+//uint8_t datalogging;
+
+#endif

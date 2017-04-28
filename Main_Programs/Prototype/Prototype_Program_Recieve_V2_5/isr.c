491f5a044af270dd4bd3d7c528fce76c03c8b775
eligilbert12 <eliyazzolino@yahoo.com>
2017-04-28 12:30:52 -0400

    poop

--- Main_Programs/Prototype/Prototype_Program_Recieve_V2_5/isr.c
+++ Main_Programs/Prototype/Prototype_Program_Recieve_V2_5/isr.c
@@ -72,7 +72,7 @@ ISR(USART1_RX_vect){
 
 	//Receieve a Byte, then pass it to VESC in main (Use flag)
 	unsigned char Recieve_Buff;
-    USART1_RX(*Recieve_Buff, 1);
+    USART1_RX(&Recieve_Buff, 1);
 
 	//USART_RX_Flag = 1;	
 	bldc_interface_uart_process_byte(Recieve_Buff);
